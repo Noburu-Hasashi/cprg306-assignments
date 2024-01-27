e86224efdd3dd47e41af41af782d618b821f5ad4
Uday Angrish <blahblahuday@gmail.com>
2024-01-26 19:05:47 -0700

    Week 2 Assignment Completed

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <h1>My name is Uday Angrish! :)</h1>
+            <p>The link to my GitHub profile is <Link href="https://github.com/Noburu-Hasashi/cprg306-assignments.git">here!</Link></p>
+        </main>
+    );
+}
