70622310f258c7544f0bfd34851cf63503c555b4
Uday Angrish <blahblahuday@gmail.com>
2024-02-05 22:32:48 -0700

    Week-3 Assignment Completed

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
     <h1 className='text-4xl font-bold pb-5'>CPRG 306: Web Development 2 - Assignments</h1>
     <ul className='text-lg'>
       <li><Link href="week-2">Week 2 Assignment</Link></li>
+      <li><Link href="week-3">Week 3 Assignment</Link></li>
     </ul>
     </div>
   </main>
