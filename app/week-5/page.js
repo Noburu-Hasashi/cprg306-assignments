62172536f62853c0a9db4a6c6f40ad1d4f6ee19c
Uday Angrish <blahblahuday@gmail.com>
2024-02-18 21:47:18 -0700

    Week 5 Assignment Completed

---
+++ app/week-5/page.js
@@ -0,0 +1,14 @@
+"use client"
+
+import ItemList from "./item-list";
+import { sortByName } from "./item-list";
+
+
+export default function Page() {
+    return (
+      <main className="bg-slate-950">
+        <h1 className="text-3xl font-bold m-2">Shopping List</h1>
+        <ItemList />
+      </main>
+    );
+  }
